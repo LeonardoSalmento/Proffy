25c40013f61b12628368cfa26eebbc42d687a5a8
LeonardoSalmento <salmento_@hotmail.com>
2020-08-03 15:56:52 -0700

    Projeto web NLW

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+import './styles.css';
+import PageHeader from '../../components/PageHeader';
+
+function TeacherForm() {
+    return (
+        <>
+        <h1>TeacherForm</h1>
+        <PageHeader title="Que incrível que você quer dá aulas." />
+        </>
+    )
+};
+
+export default TeacherForm;
