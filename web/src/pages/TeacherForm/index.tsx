b2a8635974a3ab5545e7ad8f5fc734236a893571
LeonardoSalmento <salmento_@hotmail.com>
2020-08-09 00:01:09 -0300

    Criando componentes Input, Select e Textarea, e Construindo e estilizando TeacherForm

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,16 +1,141 @@
-import React from 'react';
+import React, { useState} from 'react';
 
-
+import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import warningIcon from '../../assets/images/icons/warning.svg'
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
 
 import './styles.css';
-import PageHeader from '../../components/PageHeader';
+
 
 function TeacherForm() {
+    const [scheduleItems, setScheduleItems] = useState([
+        { week_day:0, from: '', to:''}
+    ]);
+    
+
+    function addNewScheduleItem(){
+        setScheduleItems([
+            ...scheduleItems,
+            { week_day:0, from: '', to:''}
+        ]);
+    }
+
     return (
-        <>
-        <h1>TeacherForm</h1>
-        <PageHeader title="Que incrível que você quer dá aulas." />
-        </>
+        <div id="page-teacher-form" className="container">
+            <PageHeader
+            title="Que incrível que você quer dá aulas."
+            description="O primeiro passo é preencher esse formulário de inscrição." />
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+
+                    <Input
+                        name="name"
+                        label="Nome completo"
+                    />
+
+                    <Input
+                        name="avatar"
+                        label="Avatar"
+                    />
+
+                    <Input
+                        name="whatsapp"
+                        label="WhatsApp"
+                    />
+
+                    <Textarea
+                        name="bio"
+                        label="Biografia"
+                    />
+                </fieldset>
+
+
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+
+                    <Select
+                        name="subject"
+                        label="Matéria"
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Ciências', label: 'Ciências' },
+                            { value: 'Educação Física', label: 'Educação Física' },
+                            { value: 'Física', label: 'Física' },
+                            { value: 'Geografia', label: 'Geografia' },
+                            { value: 'História', label: 'História' },
+                            { value: 'Inglês', label: 'Inglês' },
+                            { value: 'Matemática', label: 'Matemática' },
+                            { value: 'Português', label: 'Português' },
+                            { value: 'Química', label: 'Química' },
+                          ]}
+                    />
+
+                    <Input
+                        name="cost"
+                        label="Custo da sua hora por aula"
+                    />
+
+
+                </fieldset>
+
+                <fieldset>
+                    <legend>Horários disponíveis
+                        <button onClick={addNewScheduleItem}>+ Novo horário</button>
+                    </legend>
+                    {scheduleItems.map(scheduleItem=>{
+                        return (
+                            <div className="schedule-item">
+                                <Select
+                                    name="subject"
+                                    label="Matéria"
+                                    options={[
+                                        { value: 'Artes', label: 'Artes' },
+                                        { value: 'Biologia', label: 'Biologia' },
+                                        { value: 'Ciências', label: 'Ciências' },
+                                        { value: 'Educação Física', label: 'Educação Física' },
+                                        { value: 'Física', label: 'Física' },
+                                        { value: 'Geografia', label: 'Geografia' },
+                                        { value: 'História', label: 'História' },
+                                        { value: 'Inglês', label: 'Inglês' },
+                                        { value: 'Matemática', label: 'Matemática' },
+                                        { value: 'Português', label: 'Português' },
+                                        { value: 'Química', label: 'Química' },
+                                    ]}
+                                />
+
+                                <Input
+                                    name="from"
+                                    label="Das"
+                                    type="time"
+                                />
+
+                                <Input
+                                    name="to"
+                                    label="Até"
+                                    type="time"
+                                />
+                            </div>
+                        );
+                    })}
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso importante"/>
+                        Importante! <br />
+                        Preencha todos os dados
+                    </p>
+                    <button>Salvar cadastro</button>
+                </footer>
+          </main>
+        </div>
+
+
     )
 };
 
