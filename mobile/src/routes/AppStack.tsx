0c025affe24972b2eead18e9abe14c24d40e76fb
LeonardoSalmento <salmento_@hotmail.com>
2020-08-09 17:28:09 -0300

    Navegação landing

---
+++ mobile/src/routes/AppStack.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Landing from '../pages/Landing';
+import Study from '../pages/Study';
+import GiveClasses from '../pages/GiveClasses';
+
+
+const {Screen, Navigator} = createStackNavigator();
+
+function AppStack(){
+    return (
+        <NavigationContainer>
+            <Navigator screenOptions={{ headerShown: false }}>
+                <Screen name="Landing" component={Landing}/>
+                <Screen name="Study" component={Study}/>
+                <Screen name="GiveClasses" component={GiveClasses}/>
+            </Navigator>
+        </NavigationContainer>
+    )
+
+}
+
+export default AppStack;
